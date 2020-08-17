21f9c8453d5ebfbadd4ad61f08630271f29e92b1
Ildar Burkhanov <burhanoff1992@gmail.com>
2020-08-17 18:04:31 +0300

    dasdad

--- libs/main.js
+++ libs/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
-    // go
+    const burger  = $('.header-burger'),
+        body      = $('body'),
+        overlay   = $('.overlay')
+
+    burger.click(() => {
+        $(this).toggleClass('burger-active')
+
+        body.toggleClass('state-nav')
+    })
+
+    // фоновая подложка
+    overlay
+        .click(() => {
+            body.removeClass('state-nav')
+        })
+        
 })
